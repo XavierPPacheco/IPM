d42245c96424b2c964f6ee467fa808681c640d73
Xavier <x.pacheco@campus.fct.unl.pt>
2020-09-27 13:42:36 +0100

    modulos eliminados, single page app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { XavierComponent} from './xavier/xavier.component';
-import { CarolinaComponent} from './carolina/carolina.component';
-import { SofiaComponent} from './sofia/sofia.component';
-import { HomeComponent} from './home/home.component';
+
 
 const routes: Routes = [
       {path:"",pathMatch:"full", redirectTo:"/home"},
-      {path:"home",component: HomeComponent},
-      {path:"xavier",component: XavierComponent},
-      {path:"carolina",component: CarolinaComponent},
-      {path:"sofia",component: SofiaComponent},
-
+     
 ];
 
 @NgModule({
